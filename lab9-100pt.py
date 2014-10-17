4aa9c4fef2b39a442936232181bd1e1c57543ba4
ForrestBell <baseballfdude@gmail.com>
2014-10-17 16:05:55 -0500

    100 pt done

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -13,3 +13,33 @@
 # - A temperature of over 105F
 # - A temperature of over 102F and they have been sick in the last 24 hours
 # - A temperature over 100, OR they've been sick in the last 24 hours, AND they've recently travelled to West Africa.
+
+print 'How many patients are there'
+userinput1 = raw_input()
+userinput2 = raw_input().lower
+for x in range(int(userinput1)):
+    print 'What is your temperature?'
+    userinput3 = int(raw_input())
+    if userinput3>=105:
+        print 'You need to see a doctor'
+    else:
+        print 'You will be fine'
+    
+    print 'Have you been sick in the last 24 hours?'
+    userinput4 = raw_input()
+    if userinput4 == 'yes':
+        print 'You need to see a doctor'
+    else:
+        print 'You will be fine'
+    
+    print 'Have you recently travled to West Africa?'
+    userinput5 = raw_input()
+    if userinput5 == 'yes':
+        print 'You need to see a doctor'
+    else:
+        print 'You are fine'
+    if userinput3>=102 and userinput4 == 'yes':
+        print 'We need to get you checked out'
+    if userinput3>=102 and userinput4 == 'yes' and userinput5 == 'yes':
+        print 'We need to get you to a hosbital'
+    
